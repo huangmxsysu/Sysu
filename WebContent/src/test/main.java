fd4a83e637637b06841d19eb4b5bce7ceaab6ff8
mosen <995304572@qq.com>
2017-06-03 20:38:58 +0800

    test

--- WebContent/src/test/main.java
+++
@@ -1,7 +0,0 @@
-package src.test;
-
-public class main {
-public String xt(){
-	return "中大闲置";
-}
-}
---
+++ src/sysu/xianzhi/test/main.java
@@ -0,0 +1,7 @@
+package sysu.xianzhi.test;
+
+public class main {
+	public String xt(){
+		return "中大闲置";
+	}
+}
